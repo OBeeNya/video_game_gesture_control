4bf7af9f61f20ecf127dc61e41aa2afb16dd6e85
Benjamin Aubignat <benjamin.aubignat@gmail.com>
2024-07-21 20:15:35 +0200

    disowning program

--- Makefile
+++ Makefile
@@ -5,11 +5,9 @@ PYTHON = $(VENV_DIR)/Scripts/python
 
 setup: $(VENV_DIR)/Scripts/activate
 	$(PIP) install -r requirements.txt
-	$(VENV_DIR)/Scripts/deactivate
 
 launch: $(VENV_DIR)/Scripts/activate
-	$(PYTHON) app.py
-	$(VENV_DIR)/Scripts/deactivate
+	@nohup $(PYTHON) app.py > /dev/null 2>&1 &
 
 clean:
 	rm -rf $(VENV_DIR)
--- app.py
+++ app.py
@@ -20,20 +20,10 @@ class App(ctk.CTk):
         self.app1_frame.pack(side="top")
         
         self.app2_frame = ControllerOverlay(self)
-        self.app2_frame.pack(side="bottom")
-    
-    def on_closing(self):
-        
-        self.app1_frame.recognizer.close()
-        self.app1_frame.cap.release()
-        cv2.destroyAllWindows()
-        self.app2_frame.running = False
-        self.app2_frame.pygame_thread.join()
 
 if __name__ == '__main__':
     
     ctk.set_appearance_mode("Dark")
     ctk.set_default_color_theme("blue")
     app = App()
-    app.protocol("WM_DELETE_WINDOW", app.on_closing)
     app.mainloop()
